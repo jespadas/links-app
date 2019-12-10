5c9dec32df65b1e697b357488e876d2b99e8444a
Julio Espadas <julio.espadas@gmail.com>
2019-12-10 14:50:33 +0100

    passport configured for signup

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const path = require("path");
 const flash = require("connect-flash");
 const session = require("express-session");
 const MySQLStore = require("express-mysql-session");
+const passport = require('passport');
 
 const { database } = require("./keys");
 
 // initializations
 const app = express();
+require('./lib/passport');
 
 // settings
 app.set("port", process.env.PORT || 3000);
@@ -39,6 +41,8 @@ app.use(flash());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global Variables
 app.use((req, res, next) => {
