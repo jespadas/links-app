6caa61cfc3cfa7249204cabf3661d805deb413e3
Julio Espadas <julio.espadas@gmail.com>
2019-12-10 15:59:32 +0100

    signin, match pass done

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,6 +4,44 @@ const pool = require("../database");
 const helpers = require("../lib/helpers");
 
 passport.use(
+  "local.signin",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true
+    },
+    async (req, username, password, done) => {
+      const rows = await pool.query("SELECT * FROM users WHERE username = ?", [
+        username
+      ]);
+      if (rows.length > 0) {
+        const user = rows[0];
+        const validPassword = await helpers.matchPassword(
+          password,
+          user.password
+        );
+        if (validPassword) {
+          done(
+            null,
+            user,
+            req.flash("success", "Welcome " + user.username + "!")
+          );
+        } else {
+          done(null, false, req.flash("message", "Wrong password"));
+        }
+      } else {
+        return done(
+          null,
+          false,
+          req.flash("message", "The username does not exists")
+        );
+      }
+    }
+  )
+);
+
+passport.use(
   "local.signup",
   new LocalStrategy(
     {
@@ -31,6 +69,6 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser(async (id, done) => {
-    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
-    done(null, rows[0]);
+  const rows = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+  done(null, rows[0]);
 });
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -6,11 +6,26 @@ router.get("/signup", (req, res) => {
   res.render("auth/signup");
 });
 
-router.post("/signup", passport.authenticate('local.signup', {
+router.post(
+  "/signup",
+  passport.authenticate("local.signup", {
     successRedirect: "/profile",
     failureRedirect: "/signup",
     failureFlash: true
-}));
+  })
+);
+
+router.get("/signin", (req, res) => {
+  res.render("auth/signin");
+});
+
+router.post("/signin", (req, res, next) => {
+  passport.authenticate("local.signin", {
+    successRedirect: "/profile",
+    failureRedirect: "/signin",
+    failureFlash: true
+  })(req, res, next);
+});
 
 router.get("/profile", (req, res) => {
   res.send("this is your Profile");
